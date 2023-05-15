2b04a2726626c0634b084519e98b2fad2573ae2f
Regis066 <mugisharegis72@gmail.com>
2023-05-15 08:39:28 +0200

    new commit

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ cartIcon.onclick = () =>{
 closeCart.onclick = () =>{
     cart.classList.remove('active');
 }
+
+if(document.readyState == 'loading'){
+   document.addEventListener('DOMContentLoaded' , ready); 
+}else{
+    ready();
+}
+
+function ready(){
+    //Remove Items From Cart
+
+    var removeCartButton = document.getElementsByClassName('cart-remove');
+    console.log(removeCartButton)
+    for(var i = 0; i < removeCartButton.length; i++){
+        var button = removeCartButton[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+}
+
+function removeCartItem(event){
+  var buttonClicked = event.target
+  buttonClicked.parentElement.parentElement.parentElement.remove()
+}
